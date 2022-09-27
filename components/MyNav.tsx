89e9ea880c7c5271bc1e638555d6730649e7f147
paulvoid <40271180+paulvoid@users.noreply.github.com>
2022-09-27 07:10:39 +0700

    Thay đổi nhỏ

--- components/MyNav.tsx
+++ components/MyNav.tsx
@@ -13,10 +13,10 @@ const MyNav: FC = () => {
     }
     return (
         <>
-            <nav className="navbar" role="navigation" aria-label="main navigation">
+            <nav className={"navbar"} role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
                     <a className="navbar-item" href="/">
-                        <img src="/images/tst-logo-100.png" width="100" height="100" />
+                        <img src="/images/tst-logo-100.png" width="100" height="100"  alt={'Logo'}/>
                     </a>
 
                     <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
